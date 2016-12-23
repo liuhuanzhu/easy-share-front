9809c86d58d770faf217446c298b7c7e1b93ec65
liuhuanzhu <fz0207@126.com>
2016-12-23 17:03:52 +0800

    提交less插件 Signed-off-by: liuhuanzhu <fz0207@126.com>

--- easy-share/easysrc/App.vue
+++ easy-share/easysrc/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <v-footer></v-footer>
   </div>
 </template>
 <script>
+import vFooter from './components/vFooter.vue'
 export default {
     data(){
       return{
@@ -12,9 +14,22 @@ export default {
     },
     created(){
       console.log(1212);
+    },
+    components:{
+      vFooter
     }
 }
 </script>
 <style lang="scss">
+  @import './assets/css/common.css';
   @import '~mobi.css/src/mobi';
+</style>
+<style lang="less">
+  @import './assets/less/common.less';
+  #app{
+    position: absolute;
+    top: 0;right: 0;left: 0;bottom: 0;width: 100%;
+    background-color: @bgColor;
+    color: @color;
+  }
 </style>
--- easy-share/easysrc/components/vFooter.vue
+++ easy-share/easysrc/components/vFooter.vue
@@ -1,3 +1,36 @@
-<template></template>
-<style type="text/css"></style>
-<script type="text/javascript"></script>
+<template>
+	<ul class="flex-center footer">
+		<li 
+			v-for="(item,index) in items" 
+			class="unit flex-center footer_li-container flex-middle flex-vertical">
+			<span class="iconfont footer-icon" :class="item.icon"></span>{{item.link}}
+		</li>
+	</ul>
+</template>
+<style  lang="less">
+	@import '../assets/less/common.less';
+	.footer_li-container{
+		height: 1.28rem;font-size:@size;
+	}
+	.footer-icon{
+		font-size: 0.48rem;line-height: 1.4;
+		width: 100%;text-align: center;
+	}
+	.footer-link{
+		display: inline-block;
+		width: 100%;
+	}
+</style>
+<script type="text/javascript">
+	export default{
+		data(){
+			return{
+				items:[
+					{link:"买买买",icon:"icon-i-shop"},
+					{link:"赚零花",icon:"icon-i-rmb"},
+					{link:"看我的",icon:"icon-i-my"}
+				]
+			}
+		}
+	}
+</script>
